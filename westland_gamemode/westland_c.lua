98afa92c6482e05031f63bfe84312a91e4755920
MrEverlast <max.brooks83@gmail.com>
2019-12-18 15:21:10 +0100

    html

--- westland_gamemode/westland_c.lua
+++ westland_gamemode/westland_c.lua
@@ -1,6 +1,6 @@
 AddEvent("OnPackageStart", function()
     teamUI = CreateWebUI(0, 0, 0, 0, 0, 60)
-	LoadWebFile(teamUI, "http://asset/westland_gamemode/gui/team/team.html")
+	LoadWebFile(teamUI, "http://asset/westland_gamemode/gui/html/team.html")
 	SetWebAlignment(teamUI, 0.0, 0.0)
 	SetWebAnchors(teamUI, 0.0, 0.0, 1.0, 1.0)
     SetWebVisibility(teamUI, WEB_VISIBLE)
