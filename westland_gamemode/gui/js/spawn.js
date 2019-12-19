c819bc95a7e483468b3820d7b19ffb009e770e7a
miios <nathan.anne3@gmail.com>
2019-12-19 05:01:01 +0100

    design spawn menu

--- westland_gamemode/gui/js/spawn.js
+++ westland_gamemode/gui/js/spawn.js
@@ -1,3 +1,33 @@
+$(".SpawnButtonBig").on('click', () => {
+    var zone = "BigCity"
+    CallEvent("OnGetClothes", zone)
+    CallEvent("DestroyWestlandUI")
+})
+
+$(".SpawnButtonDesert").on('click', () => {
+    var zone = "DesertCity"
+    CallEvent("OnGetClothes", zone)
+    CallEvent("DestroyWestlandUI")
+})
+
+$(".SpawnButtonVillage").on('click', () => {
+    var zone = "Village"
+    CallEvent("OnGetClothes", zone)
+    CallEvent("DestroyWestlandUI")
+})
+
+$(".SpawnButtonWestern").on('click', () => {
+    var zone = "WesternTown"
+    CallEvent("OnGetClothes", zone)
+    CallEvent("DestroyWestlandUI")
+})
+
+$(".SpawnButtonOld").on('click', () => {
+    var zone = "OldTown"
+    CallEvent("OnGetClothes", zone)
+    CallEvent("DestroyWestlandUI")
+})
+
 $(".SpawnButton").on('click', () => {
     var zone = $("#spawnZone option:selected").val()
     CallEvent("OnGetClothes", zone)
